06c373fe2136a48389a095ac96a38ae53be5b9cd
cave beat <chri.bayer@gmail.com>
2013-07-15 20:43:57 +0200

    translation update

--- w2c/en/input.php
+++ w2c/en/input.php
@@ -1,46 +1,68 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" >
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="de" >
 
 <head>
-	<meta content="text/html; charset=UTF-8" http-equiv="content-type"></meta>
-	<title>Chat Server - New Key</title>
+	<meta charset="utf-8">
+	<title>RetroShare Chat Server - Schl&uuml;sselaustausch</title>
 </head>
 
 <body>
 
-	<?php
-		//echo sqlite_libversion();
-		//echo $lang[0];
-		//echo "<br>";
-		//echo phpversion();
-		//include 'manage_db_auto_clean.php';
-	?>
+<?php
+        $translation = file('translation.txt', FILE_IGNORE_NEW_LINES);
+        $transcount = count($translation);
+        for ($i = 0; $i < $transcount; $i++) {
+                $translation[$i] = substr($translation[$i], 3);
+                }
+?>
+	<h1 style="text-align: center;"><?php echo $translation[0] ; ?> </h1>
+		<p><?php echo $translation[1] ?></p>
+		
 
-	<h1 style="text-align: center;">RetroShare Chat Server</h1>
-
-		<p>This tool will help you to exchange your RetroShare key. It will give you access to chat lobbies in order to make some friends.</p>
-
-	<h2>Brief tutorial</h2>
+	<h2><?php echo $translation[2] ?></h2>
 
 		<ol>
-			<li> You enter your RetroShare key.</li>
-			<li> On the following page, you will have access to the chat server key which will give you access to chat lobbies where other members of the community are already chatting.</li>
-			<li> Chat with other people and exchange your key!</li>
+			<?php 
+				if(!($translation[3] == NULL)) { 
+				echo "<li>" . $translation[3] . "</li>" ;
+				}
+                                if(!($translation[4] == NULL)) { 
+                                echo "<li>" . $translation[4] . "</li>" ;
+                                }
+                                if(!($translation[5] == NULL)) { 
+                                echo "<li>" . $translation[5] . "</li>" ;
+                                }
+                                if(!($translation[6] == NULL)) { 
+                                echo "<li>" . $translation[6] . "</li>" ;
+                                }
+                                if(!($translation[7] == NULL)) { 
+                                echo "<li>" . $translation[7] . "</li>" ;
+                                }
+                                if(!($translation[8] == NULL)) { 
+                                echo "<li>" . $translation[8] . "</li>" ;
+                                }
+                                if(!($translation[9] == NULL)) { 
+                                echo "<li>" . $translation[9] . "</li>" ;
+                                }
+				if(!($translation[10] == NULL)) {
+                                echo "<li>" . $translation[10] . "</li>" ;
+                                }
+			?>
 		</ol>
 
 	<hr style="width: 60%; height: 2px;"></hr>
 
-	<h2>Register a new key</h2>
-
+	<h2><?php echo $translation[11] ?></h2>
 		<form method="post" action="process.php" name="process">
 			<fieldset>
 				<table border='0'>
-				<tr><td valign="top">RetroShare key:</td><td><textarea name="key" rows="10" cols="85"></textarea></td></tr>
-				<tr><td>Captcha code:<br/><img id="captcha" src="../securimage/securimage_show.php" alt="CAPTCHA Image"></img></td><td><input name="captcha_code" size="10" maxlength="6" type="text"></input><a href="#" onclick="document.getElementById('captcha').src = '../securimage/securimage_show.php?' + Math.random(); return false">[ Different Image ]</a></td></tr>
+				<tr><td valign="top"><?php echo $translation[12] ?></td><td><textarea name="key" rows="10" cols="85"></textarea></td></tr>
+				<tr><td><?php echo $translation[13] ?><br/><img id="captcha" src="../securimage/securimage_show.php" alt="CAPTCHA Image"></img></td><td><input name="captcha_code" size="10" maxlength="6" type="text"></input><a href="#" onclick="document.getElementById('captcha').src = '../securimage/securimage_show.php?' + Math.random(); return false"><?php echo $translation[14] ?></a></td></tr>
 				</table>
-				<input value="Validate" type="submit"></input>
+				<input value="<?php echo $translation[15] ?>" type="submit"></input>
 			</fieldset>
 		</form>
+
 </body>
 
 </html>
--- w2c/en/process.php
+++ w2c/en/process.php
@@ -7,6 +7,14 @@
 	</head>
  
 	<body>
+<?php
+        $translation = file('translation.txt', FILE_IGNORE_NEW_LINES);
+        $transcount = count($translation);
+        for ($i = 0; $i < $transcount; $i++) {
+                $translation[$i] = substr($translation[$i], 3);
+                }
+?>
+
 		<?php //ini_set('display_errors', 'on'); error_reporting(E_ALL);
 			//THIS version is DEVOID of SQLITE and IP CHECKING. --Jenster 01/16/12		
 			// Open config file
@@ -41,8 +49,8 @@
 			} 
 			
 			else {
-				echo "Your key is not correct! Copy-paste your ENTIRE KEY, including -----BEGIN PGP PUBLIC KEY BLOCK-----, -----END PGP PUBLIC KEY BLOCK-----, --SSLID-- and --LOCATION--.<br /><br />" ;
-				echo "Please go <a href='javascript:history.go(-1)'>back</a> and try again.";
+				echo $translation[17] ."<br /><br />" ;
+				echo $translation[18]."<a href='javascript:history.go(-1)'>". $translation[19]."</a>" . $translation[20];
 				exit();
 			}
 
@@ -58,8 +66,8 @@
 				// you should handle the error so that the form processor doesn't continue
 
 				// or you can use the following code if there is no validation or you do not know how
-				echo "The captcha code is incorrect!<br /><br />";
-				echo "Please go <a href='javascript:history.go(-1)'>back</a> and try again.";
+				echo $translation[21] . "<br /><br />";
+				echo $translation[18]."<a href='javascript:history.go(-1)'>" . $translation[19]."</a>" . $translation[20];
 				exit();
 			}
 			
@@ -84,17 +92,17 @@
 				$server_num = file_get_contents($nogui_path."/STORAGE/lobbyname.txt");
 				
 				//Print URL
-				echo "Click on the following link to add to add the chat server: ";
-				echo "<a href=\"".$server_url."\">Add chat server</a><br/>\n";
+				echo $translation[22] ;
+				echo "<a href=\"".$server_url."\">".$translation[23]."</a><br/>\n";
 
 				//Print key
-				echo "In case of problems, add manually the following key:";				
-				echo "<form name=\"select_all\">";
+				echo $translation[24] ;				
+				echo "<form name=\"select all\">";
 				echo "<p><textarea name=\"key_area\" rows=\"25\" cols=\"80\" readonly=\"readonly\">".$server_key."</textarea></p>";
-				echo "<p><input type=\"button\" value=\"Select key\" onClick=\"javascript:this.form.key_area.focus();this.form.key_area.select();\"></p>";
+				echo "<p><input type=\"button\" value=\" $translation[27]\" onClick=\"javascript:this.form.key_area.focus();this.form.key_area.select();\"></p>";
 				echo "</form>";
 				
-				echo "In a few minutes, your new friend (CHAT SERVER) should be online. You will be able to access the chat lobbies and start making friends. The actual chat lobby is <strong>".$server_num."</strong>.";
+				echo $translation[25] ." <strong>".$server_num."</strong>.";
 				
 				// Store IP
 				
