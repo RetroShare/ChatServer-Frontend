7f5d4c11e691c764d9856a9086826b88bb1b6fd8
cave beat <cave@cavebeat.org>
2015-03-15 22:01:43 +0100

    include ascii art from file

---
+++ index.php
@@ -0,0 +1,61 @@
+﻿<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+       "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<meta http-equiv="expires" content="0">
+<head>
+<title>RetroShare Chatserver hosted by Pirate Party Austria</title>
+<meta name="robots" content="noindex">
+</head>
+<body>
+<div align="center">
+<table border="1">
+    <tr>
+        <td colspan="3"><center><pre>
+ <code>                                                                                                       
+   _|_|_|                _|                            _|_|_|  _|                                       
+   _|    _|    _|_|    _|_|_|_|  _|  _|_|    _|_|    _|        _|_|_|      _|_|_|  _|  _|_|    _|_|     
+   _|_|_|    _|_|_|_|    _|      _|_|      _|    _|    _|_|    _|    _|  _|    _|  _|_|      _|_|_|_|   
+   _|    _|  _|          _|      _|        _|    _|        _|  _|    _|  _|    _|  _|        _|         
+   _|    _|    _|_|_|      _|_|  _|          _|_|    _|_|_|    _|    _|    _|_|_|  _|          _|_|_|   
+                                                                                                        </code></pre></center></td>
+    </tr>
+    <tr>
+        <td><a href="key_add.php?lang=en"><img src="img/UnitedKingdom.png" align="absmiddle"  alt="English">English<img src="img/UnitedStates.png" align="absmiddle"  alt="English"></a></td>
+        <td rowspan="6"><center>
+<?php include("ascii/pirate.txt"); ?>
+</center>
+</td>
+        <td><a href="key_add.php?lang=es"><img src="img/Spain.png" align="absmiddle"  alt="Español">Español</a></td>
+    </tr>
+    <tr>
+        <td><a href="key_add.php?lang=fr"><img src="img/France.png" align="absmiddle"  alt="Français">Français</a></td>
+        <td><a href="key_add.php?lang=de"><img src="img/Germany.png" align="absmiddle"  alt="Deutsch">Deutsch<img src="img/Austria.png" align="absmiddle"  alt="Austria"></a></td>
+    </tr>
+    <tr>
+        <td><a href="key_add.php?lang=ru"><img src="img/Russia.png" align="absmiddle"  alt="Русский">Русский</a></td>
+        <td><a href="key_add.php?lang=it"><img src="img/Italy.png" align="absmiddle"  alt="Italiano">Italiano</a></td>
+    </tr>
+    <tr>
+        <td><a href="key_add.php?lang=hu"><img src="img/Hungary.png" align="absmiddle"  alt="Magyar">Magyar</a></td>
+        <td><a href="key_add.php?lang=se"><img src="img/Sweden.png" align="absmiddle"  alt="Svenska">Svenska</a></td>
+    </tr>
+    <tr>
+        <td><a href="key_add.php?lang=pt"><img src="img/Portugal.png" align="absmiddle"  alt="Porttugal">Portuguese<img src="img/Brazil.png" align="absmiddle"  alt="Brazil"></a></td>
+        <td><a href="unknown.html"><img src="img/Anonymous.png" align="absmiddle"  alt="Anon">???</a></td> 
+    </tr>    
+    <tr>
+        <td><a href="howto.html">HowTo</td>
+        <td><a href="info.html">Info</td>    
+    </tr>
+    <tr>
+		<td><a href="https://www.piratenpartei.at/"><center><img src="img/ppmicro.gif" align="absmiddle"  alt="PirateParty"></center></a></td> 
+        <td colspan="1"><center>RetroShare Chatserver hosted by <a href="https://www.piratenpartei.at/">Austrian Pirate Party</a></center></td>
+        <td><a href="https://www.piratenpartei.at/"><center><img src="img/ppmicro.gif" align="absmiddle"  alt="PirateParty"></center></a></td> 
+    </tr>
+    <tr>
+        <td colspan="3"><center><img src="img/nyan.png" align="middle" alt="nyan"></center></td>
+    </tr>
+</table>
+</div>
+</body>
+</html>
